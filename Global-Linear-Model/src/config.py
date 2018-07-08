de27aaf4d918b92aacea11686404c40fdf6b2b92
hmjw <767257113@qq.com>
2018-07-08 12:33:50 +0800

    update result

--- Global-Linear-Model/src/config.py
+++ Global-Linear-Model/src/config.py
@@ -3,7 +3,7 @@ config = {
     'dev_data_file': './data/dev.conll',  # 开发集文件
     'test_data_file': None,  # 测试集文件
     'iterator': 100,  # 最大迭代次数
-    'shuffle': True,  # 每次迭代是否打乱数据
+    'shuffle': False,  # 每次迭代是否打乱数据
     'exitor':10,      #连续多少次迭代没有提升就退出
     'averaged': False  # 是否使用averaged percetron
 }
