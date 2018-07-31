251fb9e6d707e379be5bc4da498573092e468605
陆凯华 <kaihualu@outlook.com>
2018-07-31 10:08:38 +0800

    Update test.c

--- 汉字编码/test.c
+++ 汉字编码/test.c
@@ -3,7 +3,7 @@
 int main()
 {
     FILE * fp = fopen("data/test-UTF8","r");
-    char ch = getc(fp);
+    char ch = fgetc(fp);
     while (ch != EOF)
     {
         int test = ch & 0xf0;
@@ -12,35 +12,32 @@ int main()
             printf("%c",ch);
             for(int i=0 ; i<3 ; i++)
             {
-                ch = getc(fp);
+                ch = fgetc(fp);
                 printf("%c",ch);
             }
             printf("  ");
-            ch = getc(fp);
-            
+            ch = fgetc(fp);       
         }
         else if(test >= 0xe0 && test < 0xf0){
             printf("%c",ch);
             for(int i=0 ; i<2 ; i++)
             {
-                ch = getc(fp);
+                ch = fgetc(fp);
                 printf("%c",ch);
             }
             printf("  ");
-            ch = getc(fp);
-            continue;
+            ch = fgetc(fp);
         }
         else if(test >= 0xc0 && test <0xe0){
             printf("%c",ch);
             ch = fgetc(fp);
             printf("%c  ",ch);
             ch = fgetc(fp);
-            continue;
         }
         else
         {
             printf("%c  ",ch);
-            ch = getc(fp);
+            ch = fgetc(fp);
         }
     }
     printf("\n");
